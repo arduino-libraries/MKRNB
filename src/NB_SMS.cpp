702d804b848bd3d902a386dbcf57012574f928d4
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2020-01-17 21:11:42 +0100

    Enable SMS read in unsynch mode

--- src/NB_SMS.cpp
+++ src/NB_SMS.cpp
@@ -30,6 +30,8 @@ enum {
 NB_SMS::NB_SMS(bool synch) :
   _synch(synch),
   _state(SMS_STATE_IDLE),
+  _smsDataIndex(0),
+  _smsDataEndIndex(0),
   _smsTxActive(false)
 {
 }
@@ -111,21 +113,14 @@ int NB_SMS::endSMS()
 
 int NB_SMS::available()
 {
-  if (_incomingBuffer.length() != 0) {
-    int nextMessageIndex = _incomingBuffer.indexOf("\r\n+CMGL: ");
+  _smsDataIndex = _incomingBuffer.indexOf("+CMGL: ",_smsDataEndIndex);
 
-    if (nextMessageIndex != -1) {
-      _incomingBuffer.remove(0, nextMessageIndex + 2);
-    } else {
-      _incomingBuffer = "";
-    }
-  }
-
-  if (_incomingBuffer.length() == 0) {
+  if (_smsDataIndex == -1) {
     int r;
 
     if (_state == SMS_STATE_IDLE) {
       _state = SMS_STATE_LIST_MESSAGES;
+      _smsDataEndIndex = 0;
     }
 
     if (_synch) {
@@ -139,10 +134,11 @@ int NB_SMS::available()
     if (r != 1) {
       return 0;
     } 
+    _smsDataIndex = _incomingBuffer.indexOf("+CMGL: ",_smsDataEndIndex);
   }
 
-  if (_incomingBuffer.startsWith("+CMGL: ")) {
-    _smsDataIndex = _incomingBuffer.indexOf('\n') + 1;
+  if (_smsDataIndex != -1) {
+    _smsDataIndex = _incomingBuffer.indexOf('\n',_smsDataIndex) + 1;
 
     _smsDataEndIndex = _incomingBuffer.indexOf("\r\n+CMGL: ");
     if (_smsDataEndIndex == -1) {
@@ -210,6 +206,8 @@ void NB_SMS::flush()
 {
   int smsIndexStart = _incomingBuffer.indexOf(' ');
   int smsIndexEnd = _incomingBuffer.indexOf(',');
+
+  _dataEndIndex = 0;
 
   if (smsIndexStart != -1 && smsIndexEnd != -1) {
     while (MODEM.ready() == 0);
