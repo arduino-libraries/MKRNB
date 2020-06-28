22d1b56ac6640baa2f00356f62d96d4ecc71dea7
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2020-06-28 23:31:43 +0200

    Backout changes to NB.cpp

--- src/NB.cpp
+++ src/NB.cpp
@@ -124,12 +124,13 @@ int NB::isAccessAlive()
 
 bool NB::shutdown()
 {
-  // Attempt AT command shutdown
-  if (_state == NB_READY && MODEM.shutdown()) {
-    _state = NB_OFF;
-    return true;
+  if (_state == NB_READY) {
+    MODEM.send("AT+CPWROFF");
+    MODEM.waitForResponse(40000);
   }
-  return false;
+  MODEM.end();
+  _state = NB_OFF;
+  return true;
 }
 
 bool NB::secureShutdown()
@@ -430,7 +431,7 @@ void NB::setTimeout(unsigned long timeout)
   _timeout = timeout;
 }
 
-unsigned long NB::getLocalTime()
+unsigned long NB::getTime()
 {
   String response;
 
@@ -453,9 +454,9 @@ unsigned long NB::getLocalTime()
     time_t delta = ((response.charAt(26) - '0') * 10 + (response.charAt(27) - '0')) * (15 * 60);
 
     if (response.charAt(25) == '-') {
-      result -= delta;
-    } else if (response.charAt(25) == '+') {
       result += delta;
+    } else if (response.charAt(25) == '+') {
+      result -= delta;
     }
 
     return result;
@@ -464,7 +465,7 @@ unsigned long NB::getLocalTime()
   return 0;
 }
 
-unsigned long NB::getTime()
+unsigned long NB::getLocalTime()
 {
   String response;
 
